f1a630654e2d9a650354bb689be9407254c73b7d
Filippo DallOlio <fdallolio@brocku.ca>
2024-02-20 20:48:32 -0500

    .

--- R/vec_slice.R
+++ R/vec_slice.R
@@ -6,21 +6,43 @@ NULL
 #' @rdname slicing
 #' @export
 #'
-vec_slice_pos <- function(x, i,..., rev = FALSE){
-  if(rev) i <- sort(vctrs::vec_size(x) + 1 - abs(i))
-  vctrs::vec_slice(x, i, ...)
+# vec_slice_pos <- function(x, i,..., rev = FALSE){
+  # if(rev) i <- sort(vctrs::vec_size(x) + 1 - abs(i))
+#   vctrs::vec_slice(x, i, ...)
+# }
+vec_slice_pos <- function(x, i, ..., .sort = TRUE){
+  n <- vctrs::vec_size(x)
+  p1 <- sign(i) > 0
+  p0 <- !p1
+  i[p0] <- n + 1 + i[p0]
+  if(.sort){
+    i <- sort(i)
+  }
+  x[i]
 }
 #'
 #' @rdname slicing
 #' @export
 #'
+vslice <-  vec_slice_pos
+#'
+#' @rdname slicing
+#' @export
+#
+lslice <- function(x, i) purrr::map(x, ~ vslice(.x, i))
+#'
+#' @rdname slicing
+#' @export
+#'
 vec_slice_head <- function(x, n){
-  vctrs::vec_slice(x, seq_len(n))
+  x[seq_len(n)]
 }
 #'
 #' @rdname slicing
 #' @export
 #'
 vec_slice_tail <- function(x, n){
-  rev(vctrs::vec_slice(rev(x), seq_len(n)))
+  i <- sort(vec_size(x) + 1 - seq_len(n))
+  x[i]
 }
+
